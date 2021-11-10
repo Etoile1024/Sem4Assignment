7ffbf5ce046895fb3668a0b6c9f9ceed54006152
エトワール <74129373+Etoile1024@users.noreply.github.com>
2021-11-10 09:58:21 +0800

    Update factory pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,6 @@ package com.company;
 
 import com.company.command.*;
 import com.company.exception.UnknownCommandException;
-import com.company.factory.CommandFactory;
 import com.company.singleton.ScannerSingleton;
 
 import java.util.*;
@@ -12,12 +11,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = ScannerSingleton.getInstance().getScanner();
-        CommandFactory cmdFactory = new CommandFactory();
         while (true) {
             help();
             try {
                 String input = scanner.nextLine();
-                Command cmd = cmdFactory.getCommand(input);
+                Command cmd = createCommand(input);
                 cmd.execute();
             } catch (UnknownCommandException ex) {
                 System.out.println("Invalid command!");
@@ -26,6 +24,29 @@ public class Main {
 
         }
 
+    }
+
+    public static Command createCommand(String input) throws UnknownCommandException {
+        switch (input) {
+            default:
+                throw new UnknownCommandException();
+            case "c":
+                return new CreateToyCommand();
+            case "d":
+                return new DisplayToyCommand();
+            case "p":
+                return new PurchaseToyCommand();
+            case "s":
+                return new SellToyCommand();
+            case "l":
+                return new DisplayStackCommand();
+            case "u":
+                return new UndoCommand();
+            case "r":
+                return new RedoCommand();
+            case "x":
+                return new ExitCommand();
+        }
     }
 
     public static void help() {
--- src/com/company/command/CreateToyCommand.java
+++ src/com/company/command/CreateToyCommand.java
@@ -1,7 +1,6 @@
 package com.company.command;
 
 import com.company.exception.UnknownToyTypeException;
-import com.company.factory.ToyProductFactory;
 import com.company.product.*;
 import com.company.singleton.CommandsSingleton;
 import com.company.singleton.ProductsSingleton;
@@ -28,9 +27,8 @@ public class CreateToyCommand implements Command {
     }
 
     private void createToyByType(String toyType) {
-        ToyProductFactory toyFactory = new ToyProductFactory();
         try {
-            toyProduct = toyFactory.getToyProduct(toyType);
+            toyProduct = createToyProduct(toyType);
             ProductsSingleton.getInstance().addProduct(toyProduct);
             CommandsSingleton.getInstance().pushUndoStack(this);
             System.out.println("New toy product record created.");
@@ -41,6 +39,46 @@ public class CreateToyCommand implements Command {
         } catch (Exception ex) {
             System.out.println("Input error when create toy, please input again!");
             createToyByType(toyType);
+        }
+    }
+
+    public ToyProduct createToyProduct(String toyType) throws UnknownToyTypeException {
+        Scanner scanner = ScannerSingleton.getInstance().getScanner();
+        String input;
+        int id;
+        String name;
+
+        switch (toyType) {
+            case "ro":
+                System.out.println("Enter id:");
+                input = scanner.nextLine();
+                id = Integer.parseInt(input);
+
+                System.out.println("Enter name:");
+                input = scanner.nextLine();
+                name = input;
+
+                System.out.println("Enter height(cm):");
+                input = scanner.nextLine();
+                double height = Double.parseDouble(input);
+
+                return new Robot(id, name, height);
+            case "rc":
+                System.out.println("Enter id:");
+                input = scanner.nextLine();
+                id = Integer.parseInt(input);
+
+                System.out.println("Enter name:");
+                input = scanner.nextLine();
+                name = input;
+
+                System.out.println("Enter maximum speed(km/hr):");
+                input = scanner.nextLine();
+                double maxSpeed = Double.parseDouble(input);
+
+                return new RemoteControlCar(id, name, maxSpeed);
+            default:
+                throw new UnknownToyTypeException();
         }
     }
 
